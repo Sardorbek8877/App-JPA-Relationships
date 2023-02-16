9476491b2c51c7d2813d54bae38a42946ba52b1f
Sardorbek8877 <sardorbeksafarov8877@gmail.com>
2023-02-16 13:57:06 +0100

    first commit

---
+++ src/main/java/uz/bek/appjparelationships/entity/Student.java
@@ -0,0 +1,43 @@
+package uz.bek.appjparelationships.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @OneToOne  //ONE student TO ONE address
+    private Address address;
+
+    @ManyToMany
+    private List<Subject> subjects;
+}
+
+
+
+
+
+
+
+
+
+
+
+
