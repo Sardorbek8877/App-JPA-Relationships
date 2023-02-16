7e0c3ed7e5948b60aa2922c0d7e20d9619d0e54c
Sardorbek8877 <sardorbeksafarov8877@gmail.com>
2023-02-16 16:55:23 +0100

    create

--- src/main/java/uz/bek/appjparelationships/controller/UniversityController.java
+++ src/main/java/uz/bek/appjparelationships/controller/UniversityController.java
@@ -1,10 +1,13 @@
 package uz.bek.appjparelationships.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import uz.bek.appjparelationships.entity.Address;
 import uz.bek.appjparelationships.entity.University;
+import uz.bek.appjparelationships.payload.UniversityDto;
 import uz.bek.appjparelationships.repository.AddressRepository;
 import uz.bek.appjparelationships.repository.UniversityRepository;
 
@@ -26,5 +29,23 @@ public class UniversityController {
     }
 
     //CREATE
+    @RequestMapping(value = "/university", method = RequestMethod.POST)
+    public String addUniversity(@RequestBody UniversityDto universityDto){
 
+        // CREATE NEW ADDRESS
+        Address address = new Address();
+        address.setCity(universityDto.getCity());
+        address.setDistrict(universityDto.getDistrict());
+        address.setStreet(universityDto.getStreet());
+
+        Address saveAddress = addressRepository.save(address);
+
+        University university = new University();
+        university.setName(universityDto.getName());
+        university.setAddress(saveAddress);
+
+        universityRepository.save(university);
+
+        return "University added";
+    }
 }
